cd624b5dcbe4bebe6ea0a388d83765eb9e5787f0
Brittany Ann <britkos@gmail.com>
2016-12-27 18:25:31 -0700

    added schedule, updated styling

--- js/common.js
+++ js/common.js
@@ -1,18 +1,4 @@
 
-/*
- * 		Spring 2016 Hackathon Slideshow
- */
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("slideshow_slide");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+$("#nav_icon").click(function(){
+	$(".navigation_large").slideToggle();
+});
