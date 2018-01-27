9bf2ebbdd085a498577203170530f09391fe3a28
Brittany Ann <britkos@gmail.com>
2018-01-27 00:50:52 -0700

    adding more track info

--- js/common.js
+++ js/common.js
@@ -127,26 +127,3 @@ function navIconToggle() {
 /* 
  *  END MOBILE NAVIGATION
  */
-
-
-
-/*
- * 	Spring 2016 Hackathon Slideshow
- */
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("slideshowSlide");
-    for (i = 0; i < x.length; i++) {
-    	x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 3000); // Change image every 2 seconds
-}
-/*
- * 	END Spring 2016 Hackathon Slideshow
- */
